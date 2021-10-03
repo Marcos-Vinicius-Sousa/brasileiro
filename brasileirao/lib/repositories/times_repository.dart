8dbe9b02bde493d00d32da2d692720581777272c
Marcos Vinicius Sousa do Rosario <55248963+Marcos-Vinicius-Sousa@users.noreply.github.com>
2021-10-03 12:17:02 -0300

    trocando o endereço das imagens dos times

--- brasileirao/lib/repositories/times_repository.dart
+++ brasileirao/lib/repositories/times_repository.dart
@@ -27,96 +27,96 @@ class TimesRepository extends ChangeNotifier{
     Time(
         nome:'Corinthians',
         pontos: 75,
-        brasao:'https://e.imguol.com/futebol/brasoes/corinthians.png',
+        brasao:'https://logodetimes.com/times/corinthians/logo-do-corinthians-256.png',
         cor: Colors.black
     ),
     Time(
     nome:'Palmeiras',
     pontos: 68,
-    brasao:'https://e.imguol.com/futebol/brasoes/palmeiras.png',
+    brasao:'https://logodetimes.com/times/palmeiras/logo-palmeiras-256.png',
         cor: Colors.green[800]
     ),
     Time(
       nome:'Flamengo',
       pontos: 68,
-      brasao:'https://e.imguol.com/futebol/brasoes/flamengo.png', cor: Colors.red[900]
+      brasao:'https://logodetimes.com/times/flamengo/logo-flamengo-256.png', cor: Colors.red[900]
     ),
     Time(
     nome:'Atlético-MG',
     pontos: 67,
-    brasao:'https://e.imguol.com/futebol/brasoes/atletico-mg.png',
+    brasao:'https://logodetimes.com/times/atletico-mineiro/logo-atletico-mineiro-256.png',
         cor: Colors.black
     ),
     Time(
     nome:'Internacional',
     pontos: 65,
-    brasao:'https://e.imguol.com/futebol/brasoes/internacional.png',
+    brasao:'https://logodetimes.com/times/internacional/logo-internacional-256.png',
         cor: Colors.red
     ),
     Time(
     nome:'Fluminense',
     pontos: 62,
-    brasao:'https://e.imguol.com/futebol/brasoes/fluminense.png',
+    brasao:'https://logodetimes.com/times/fluminense/logo-fluminense-256.png',
         cor: Colors.red[900]
     ),
     Time(
     nome:'Fortaleza',
     pontos: 61,
-    brasao:'https://e.imguol.com/futebol/brasoes/fortaleza.png',
+    brasao:'https://logodetimes.com/times/fortaleza/logo-fortaleza-256.png',
         cor: Colors.blue[800]
     ),
     Time(
     nome:'São Paulo',
     pontos: 57,
-    brasao:'https://e.imguol.com/futebol/brasoes/sao-paulo.png',
+    brasao:'https://logodetimes.com/times/sao-paulo/logo-sao-paulo-256.png',
         cor: Colors.red
     ),
     Time(
     nome:'Gremio',
     pontos: 56,
-    brasao:'https://e.imguol.com/futebol/brasoes/gremio.png',
+    brasao:'https://logodetimes.com/times/gremio/logo-gremio-256.png',
         cor: Colors.blue[700]
     ),
     Time(
     nome:'Bahia',
     pontos: 55,
-    brasao:'https://e.imguol.com/futebol/brasoes/bahia.png',
+    brasao:'https://logodetimes.com/times/bahia/logo-bahia-256.png',
         cor: Colors.blue
     ),
     Time(
     nome:'Chapecoense',
     pontos: 54,
-    brasao:'https://e.imguol.com/futebol/brasoes/chapecoense.png',
+    brasao:'https://logodetimes.com/times/chapecoense/logo-chapecoense-256.png',
         cor: Colors.green
     ),
     Time(
     nome:'Santos',
     pontos: 52,
-    brasao:'https://e.imguol.com/futebol/brasoes/santos.png',
+    brasao:'https://logodetimes.com/times/santos/logo-santos-256.png',
         cor: Colors.black
     ),
     Time(
     nome:'Goias',
     pontos: 49,
-    brasao:'https://e.imguol.com/futebol/brasoes/goias.png',
+    brasao:'https://logodetimes.com/times/goias/logo-goias-novo-256.png',
         cor: Colors.green
     ),
     Time(
     nome:'Vasco',
     pontos: 48,
-    brasao:'https://e.imguol.com/futebol/brasoes/vasco.png',
+    brasao:'https://logodetimes.com/times/vasco-da-gama/logo-vasco-da-gama-256.png',
         cor: Colors.black
     ),
     Time(
     nome:'Botafogo',
     pontos: 46,
-    brasao:'https://e.imguol.com/futebol/brasoes/botafogo.png',
+    brasao:'https://logodetimes.com/times/botafogo/logo-botafogo-256.png',
         cor: Colors.black
     ),
     Time(
     nome:'Atlético-GO',
     pontos: 44,
-    brasao:'https://e.imguol.com/futebol/brasoes/atletico-go.png',
+    brasao:'https://logodetimes.com/times/atletico-goianiense/logo-atletico-goianiense-com-estrela-256.png',
         cor: Colors.red[900]
     ),
 
